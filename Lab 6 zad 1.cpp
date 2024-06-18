b731ad9ea0d825146aa1f113f5915bd60e86fde4
Paweł <121982293+Nealz1@users.noreply.github.com>
2024-06-18 20:34:03 +0200

    Add files via upload

---
+++ Lab 6 zad 1.cpp
@@ -0,0 +1,384 @@
+//============================================================================
+//                  Algorytmy i Struktury Danych
+// Zadanie 6.1
+// Drzewo BST
+//
+//  WCY21IX4S0  Miksiewicz Pawel
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+//SPACJA O WIELKOSCI 10 (POTRZEBNE DO RYSOWANIA DRZEWA)
+#define SPACE 10
+
+using namespace std;
+
+//STRUKTURA DRZEWA BST
+struct DrzewoBST
+{
+	int wartosc;
+  	struct DrzewoBST *lewo;
+  	struct DrzewoBST *prawo;
+};
+
+//FUNCKJA ZEBY SPRAWDZIC CZY DRZEWO JEST PUSTE
+int CzyPusteDrzewoBST(struct DrzewoBST *korzen)
+{
+    if (korzen == NULL)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+//FUNCKJA SPRAWDA CZY SZUKANA DANA JEST W DRZEWIE, JESLI TAK, ZWROCI TRUE, JESLI NIE, ZWROCI FALSE
+bool CzyJestWDrzewie(struct DrzewoBST* korzen, int dana)
+{
+    if(korzen == NULL)
+    {
+        return false;
+    }
+    if(korzen->wartosc == dana)
+    {
+        return true;
+    }
+    bool szukaj1 = CzyJestWDrzewie(korzen->lewo, dana);
+    if(szukaj1)
+    {
+        return true;
+    }
+
+    bool szukaj2 = CzyJestWDrzewie(korzen->prawo, dana);
+    return szukaj2;
+}
+
+//FUNKCJA NA STWORZENIE PIERWSZEGO WEZLU
+struct DrzewoBST* Wezel(int dana)
+{
+    struct DrzewoBST* tymczasowa = (struct DrzewoBST*)malloc(sizeof(struct DrzewoBST));
+
+    tymczasowa->wartosc = dana;
+    tymczasowa->lewo = NULL;
+    tymczasowa->prawo = NULL;
+
+    return tymczasowa;
+};
+
+//FUNCKJA NA DODAWANIE DO DRZEWA
+struct DrzewoBST* DodajDoDrzewa(struct DrzewoBST* korzen, int dana)
+{
+    if(korzen == NULL)
+    {
+        return Wezel(dana);
+    }
+    if(dana < korzen->wartosc)
+    {
+        korzen->lewo = DodajDoDrzewa(korzen->lewo, dana);
+    }
+    else if(dana > korzen->wartosc)
+    {
+        korzen->prawo = DodajDoDrzewa(korzen->prawo, dana);
+    }
+    return korzen;
+};
+
+
+//FUNCKJA NA WYSZUKIWANIE DANEGO ELEMENTU Z DRZEWA I WYPISANIE SCIEZKI DO SZUKANEJ DANEJ
+struct DrzewoBST* Wyszukaj(struct DrzewoBST *korzen, int dana)
+{
+    if(korzen == NULL || korzen->wartosc == dana)
+    {
+        cout << korzen ->wartosc;
+        return korzen;
+    }
+    else if(dana > korzen->wartosc)
+    {
+        cout << korzen->wartosc << "-->";
+        return Wyszukaj(korzen->prawo, dana);
+    }
+    else
+    {
+        cout << korzen->wartosc << "-->";
+        return Wyszukaj(korzen->lewo, dana);
+    }
+};
+
+//FUNKCJA NA ZNALEZENIE NAJMNIEJSZEGO WEZLA (POTRZEBNE DO USUWANIA)
+struct DrzewoBST* NajmniejszyWezel(struct DrzewoBST* drzewko)
+{
+    struct DrzewoBST* tymczasowa = drzewko;
+
+    while(tymczasowa && tymczasowa->lewo != NULL)
+    {
+        tymczasowa = tymczasowa->lewo;
+    }
+    return tymczasowa;
+};
+
+//FUNKCJA NA ZNALEZIENIE MAKSA W DRZEWIE
+struct DrzewoBST* NajwiekszyWezel(struct DrzewoBST* drzewko)
+{
+    struct DrzewoBST* tymczasowa = drzewko;
+
+    while(tymczasowa && tymczasowa->prawo != NULL)
+    {
+        tymczasowa = tymczasowa->prawo;
+    }
+    return tymczasowa;
+};
+
+//FUNKCJA NA USUNIECIE DANEGO ELEMENTU Z DRZEWA I ZWROCENIE STRUKTURY PO USUNIECIU DRZEWA
+struct DrzewoBST* UsunWezel(struct DrzewoBST* korzen, int dana)
+{
+    if(korzen == NULL)
+    {
+        return korzen;
+    }
+    if(dana < korzen->wartosc)
+    {
+        korzen->lewo = UsunWezel(korzen->lewo, dana);
+    }
+    else if(dana > korzen->wartosc)
+    {
+        korzen->prawo = UsunWezel(korzen->prawo, dana);
+    }
+    else
+    {
+        if(korzen->lewo == NULL && korzen->prawo == NULL)
+        {
+            return NULL;
+        }
+        else if(korzen->lewo == NULL)
+        {
+            struct DrzewoBST* tymczasowa = korzen->prawo;
+            free(korzen);
+            return tymczasowa;
+        }
+        else if(korzen->prawo == NULL)
+        {
+            struct DrzewoBST* tymczasowa = korzen->lewo;
+            free(korzen);
+            return tymczasowa;
+        }
+        
+        struct DrzewoBST* tymczasowa = NajmniejszyWezel(korzen->prawo);
+        korzen->wartosc = tymczasowa->wartosc;
+        korzen->prawo = UsunWezel(korzen->prawo, tymczasowa->wartosc);
+    }
+    return korzen;
+};
+
+//FUNCKJA NA RYSOWANIE DRZEWA WRAZ ODSTEPAMI ABY DRZEWO LADNIE WYGLADALO
+void RysowanieDrzewa(struct DrzewoBST* korzen, int spacja)
+{
+    if (korzen == NULL)
+    {
+        return;
+    }
+    spacja = spacja + SPACE;
+    RysowanieDrzewa(korzen->prawo, spacja);
+    cout<<endl;
+    for (int i = SPACE; i < spacja; i++)
+    {
+        cout<<" ";
+    }
+    cout<<korzen->wartosc<<endl;
+    RysowanieDrzewa(korzen->lewo, spacja);
+}
+
+//FUNKCJA ABY WYPISAC W STYLU VLR (PRE-ORDER, PREFIKSOWE)
+void Preorder(struct DrzewoBST *r)
+{
+    if (r == NULL)
+    {
+		return;
+	}
+
+    cout << r -> wartosc << " ";
+    Preorder(r -> lewo);
+    Preorder(r -> prawo);
+}
+
+//FUNKCJA ABY WYPISAC W STYLU LVR (IN-ORDER, INFIKSOWE)
+void Inorder(struct DrzewoBST *r)
+{
+    if (r == NULL)
+    {
+    	return;
+	}
+    Inorder(r -> lewo);
+    cout << r -> wartosc << " ";
+    Inorder(r -> prawo);
+}
+
+//FUNKCJA ABY WYPISAC W STYLU LRV (POST-ORDER, POSTFIKSOWE)
+void Postorder(struct DrzewoBST *r)
+{
+    if (r == NULL)
+    {
+		return;
+	}
+    Postorder(r -> lewo);
+    Postorder(r -> prawo);
+    cout << r -> wartosc << " ";
+}
+
+//FUNCKJA USUWA DANE Z DRZEWA
+void UsunCaleDrzewoBST(struct DrzewoBST* korzen)
+{
+    if (korzen == NULL)
+    {
+       return;
+    }
+
+    UsunCaleDrzewoBST(korzen->lewo);
+    UsunCaleDrzewoBST(korzen->prawo);
+    free(korzen);
+}
+
+//FUNKCJA WYWOLUJE INNA FUNKCJE DO USUWANIE DRZEWA I ZAMIENIA USUNIETE ZNAKI NA NULL
+void UsunDrzewoBST(struct DrzewoBST** korzen)
+{
+    UsunCaleDrzewoBST(*korzen);
+    *korzen = NULL;
+}
+
+//MAIN
+int main()
+{
+	struct DrzewoBST* korzen = NULL;
+	int opcja, wartosc;
+	int petla = 1;
+	
+	while(petla)
+	{
+		system("cls");
+		cout << "Drzewo BST" << endl;
+		cout << "Wybierz operacje:" << endl;
+    	cout << "1. Dodaj wezel z klawiatury." << endl;
+    	cout << "2. Usun wezel." << endl;
+    	cout << "3. Szukaj wezla." << endl;
+    	cout << "4. Usun cale drzewo." << endl;
+    	cout << "5. Rysuj drzewo." << endl;
+   		cout << "6. Wypisanie wezlow drzewa w kolejnosci VLR, LVR, LRV." << endl;
+   		cout << "0. Koniec dzialania." << endl;
+		cout << endl;
+		
+    	cin >> opcja;
+
+		system("cls");
+    	switch (opcja)
+		{
+    	case 0: //KONIEC PROGRAMU
+            	cout << "Koniec pracy programu." << endl;
+            	exit(0);
+      			break;
+    	case 1: //DODAWANIE DO WEZLA Z KLAWIATURY
+      			cout << "1. Dodawanie wezla\n\nPodaj liczbe:" << endl;
+	      		cin >> wartosc;
+                korzen = DodajDoDrzewa(korzen, wartosc);
+	      		cout<<endl;
+    			break;
+    	case 2: //USUWANIE DANEGO WEZLA Z DRZEWA
+    			if(CzyPusteDrzewoBST(korzen) == 1)
+    			{
+    				cout << "Drzewo jest puste. Najpierw dodaj liczby do drzewa." << endl << endl;
+				}
+				else
+				{
+                    cout << "2. Usun wezel \n\nPodaj wezel do usuniecia" << endl;
+                    cin >> wartosc;
+                    cout << endl;
+                    if(CzyJestWDrzewie(korzen, wartosc))
+                    {
+                        korzen = UsunWezel(korzen, wartosc);
+                        cout << "Usunieto pomyslnie wezel." << endl << endl;
+                    }
+                    else
+                    {
+                        cout << "Wprowadzony wezel nie znajduje sie w liscie." << endl << endl;
+                    }
+
+      			}
+      			break;
+    	case 3: //SZUKANIE WEZLA W DRZEWIE
+    			if(CzyPusteDrzewoBST(korzen) == 1)
+    			{
+    				cout << "Drzewo jest puste. Najpierw dodaj liczby do drzewa." << endl << endl;
+				}
+				else
+				{
+                    cout << "3. Szukaj wezla\n\nPodaj szukany wezel i wyswietl sciezke do niego." << endl;
+                    cin >> wartosc;
+                    cout << endl;
+                    if(CzyJestWDrzewie(korzen, wartosc))
+                    {
+                    	cout << "Sciezka:" << endl;
+                        Wyszukaj(korzen, wartosc);
+                        cout << endl << endl;
+                    }
+                    else
+                    {
+                        cout << "Podany wezel nie znajduje sie w drzewie." << endl << endl;
+                    }
+                }
+      			break;
+      	case 4: //USUWANIE CALEGO DRZEWA
+                if(CzyPusteDrzewoBST(korzen) == 1)
+    			{
+    				cout << "Drzewo jest puste. Najpierw dodaj liczby do drzewa." << endl << endl;
+				}
+                else
+                {
+                    cout << "4. USUWANIE CALEGO DRZEWA." << endl << endl;
+                    UsunDrzewoBST(&korzen);
+                    cout << "Pomyslnie usunieto cale drzewo." << endl << endl;
+                }
+      			break;
+      	case 5: //RYSOWANIE DRZEWA
+      			if(CzyPusteDrzewoBST(korzen) == 1)
+    			{
+    				cout << "Drzewo jest puste. Najpierw dodaj liczby do drzewa." << endl << endl;
+				}
+				else
+				{
+      			cout << "5. RYSOWANIE DRZEWA." << endl << endl;
+     			RysowanieDrzewa(korzen, 0);
+      			cout << endl;
+      			}
+      			break;
+    	case 6: //WYPISYWANIE WEZLOW DRZEWA W KOLEJNOSCI VLR, LVR, LRV
+    			if(CzyPusteDrzewoBST(korzen) == 1)
+    			{
+    				cout << "Drzewo jest puste. Najpierw dodaj liczby do drzewa." << endl << endl;
+				}
+				else
+				{
+    			cout << "6. Wypisanie wezlow drzewa w kolejnosci VLR, LVR, LRV." << endl << endl;
+    			cout << "VLR - pre-order, przejscie wzdluzne, prefiksowe" << endl;
+      	      	Preorder(korzen);
+      	      	cout << endl;
+    			cout << endl << "LVR - in-order, przejscie poprzeczne, infiksowe" << endl;
+      	      	Inorder(korzen);
+      	      	cout << endl;
+    			cout << endl << "LRV - post-oder, przejscie wsteczne, postfiksowe" << endl;
+      	     	Postorder(korzen);
+      	     	cout << endl << endl;
+      	     	}
+    			break;
+    	default:
+      			cout << "Wpisano bledny numer operacji. Sprobuj ponownie." << endl;
+    	}
+    system("pause");
+};
+
+return 0;
+}
+
---
+++ Lab 6 zad 2.cpp
@@ -0,0 +1,361 @@
+//============================================================================
+//                  Algorytmy i Struktury Danych
+// Zadanie 6.2
+// Kopiec
+//
+//  WCY21IX4S0  Miksiewicz Pawel
+//============================================================================
+
+#include <stdlib.h>
+#include <malloc.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+//STRUKTURA DEFINIUJACA STOG
+struct Stog
+{
+    int liczba;
+    struct Stog *ojciec;
+    struct Stog *lewo;
+    struct Stog *prawo;
+};
+
+//STRUKTURA NA KOPIEC
+struct Kopiec
+{
+	int poziom;
+    int IloscElementow;
+    struct Stog ***wysokosc;
+};
+
+//DODAWANIE PIERWSZEGO WEZLA
+struct Stog *heap(int liczba)
+{
+    struct Stog *element = (struct Stog*)malloc(sizeof(struct Stog));
+    
+    element->liczba = liczba;
+    element->ojciec = NULL;
+    element->lewo = NULL;
+    element->prawo = NULL;
+    
+    return element;
+};
+
+
+struct Kopiec *DrzewoKopiec(int maksimum)
+{
+    struct Kopiec *dana = (struct Kopiec *)malloc(sizeof(struct Kopiec));
+    
+    int i;
+    dana->poziom = -1;
+    dana->IloscElementow = 0;
+    
+    dana->wysokosc = (struct Stog ***)malloc(sizeof(struct Stog **) * maksimum);
+    
+    for (i = 0; i < maksimum; i++)
+    {
+        dana->wysokosc[i] = NULL;
+    }
+    return dana;
+};
+
+//FUNCKJA NA ZWRACANIE POTEG LICZBY 2
+int PotegaLiczby2(int n)
+{
+    int i;
+    int wynik;
+    wynik = 1;
+    
+    for (i = 0; i < n; i++)
+    {
+        wynik = 2 * wynik;
+    }
+    return wynik;
+}
+
+//WYPISANIE DO KONSOLI KOPCA
+void Wyswietl(struct Kopiec *korzen)
+{
+    int poziom, i;
+    
+    for (poziom = 0; poziom <= korzen->poziom; poziom++)
+    {
+        for (i = 0; i < PotegaLiczby2(poziom); i++)
+        {
+            if (korzen->wysokosc[poziom][i] != NULL)
+			{
+                printf("%d ", korzen->wysokosc[poziom][i]->liczba);
+            }
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+//FUNKCJA NA DODANIE NOWEJ GALEZI
+void DodajGalaz(struct Kopiec *korzen, int poziom)
+{   int i;
+    if (korzen->wysokosc[poziom] == NULL)
+    {
+        korzen->wysokosc[poziom] = (struct Stog **)malloc(sizeof(struct Stog *) * PotegaLiczby2(poziom));
+        for(i=0;i<PotegaLiczby2(poziom);i++)
+		{
+            korzen->wysokosc[poziom][i] == NULL;
+        }
+        korzen->poziom = poziom;
+        korzen->IloscElementow = 0;
+    }
+}
+
+//FUNKCJA ABY WYPISAC W STYLU VLR (PRE-ORDER, PREFIKSOWE)
+void Preorder(struct Stog *wezel)
+{
+    if (wezel)
+    {
+        printf("%d ", wezel->liczba);
+        Preorder(wezel->lewo);
+        Preorder(wezel->prawo);
+    }
+}
+
+//FUNKCJA ABY WYPISAC W STYLU LVR (IN-ORDER, INFIKSOWE)
+void Inorder(struct Stog *wezel)
+{
+    if (wezel)
+    {
+        Inorder(wezel->lewo);
+        printf("%d ", wezel->liczba);
+        Inorder(wezel->prawo);
+    }
+}
+
+//FUNKCJA ABY WYPISAC W STYLU LRV (POST-ORDER, POSTFIKSOWE)
+void Postorder(struct Stog *wezel)
+{
+    if (wezel)
+    {
+        Postorder(wezel->lewo);
+        Postorder(wezel->prawo);
+        printf("%d ", wezel->liczba);
+    }
+}
+
+//FUNKCJA NA WYBRANIE SPOSOBU WYSWIETLANIA
+void RodzajWyswietlania(struct Kopiec *korzen, int RodzajWyswietlania)
+{
+    struct Stog *temp;
+    temp=korzen->wysokosc[0][0];
+    if (RodzajWyswietlania==1)
+    {
+        Preorder(temp);
+    }
+    
+    else if (RodzajWyswietlania==2)
+    {
+        Inorder(temp);
+    }
+    
+    else if (RodzajWyswietlania==3)
+    {
+        Postorder(temp);
+    }
+}
+
+
+//FUNKCJA NA USUNIECIE DANEJ GALEZI
+void Usungalaz(struct Kopiec *korzen, int poziom)
+{
+    korzen->poziom = poziom - 1;
+    if (korzen->poziom >= 0)
+    {
+        korzen->IloscElementow = PotegaLiczby2(korzen->poziom);
+    }
+}
+
+//FUNCKJA NA DODAWANIE DO KOPCA
+void DodajDoKopca(struct Kopiec *korzen, struct Stog *element)
+{
+    struct Stog *element2;
+    struct Stog *ojciec;
+    int liczba;
+    if ((korzen->poziom == -1) || (korzen->IloscElementow >= PotegaLiczby2(korzen->poziom)))
+    {
+        DodajGalaz(korzen, korzen->poziom + 1);
+    }
+    korzen->wysokosc[korzen->poziom][korzen->IloscElementow] = element;
+    korzen->IloscElementow++;
+    if (korzen->poziom > 0)
+    {
+        ojciec = korzen->wysokosc[korzen->poziom - 1][(korzen->IloscElementow - 1) / 2];
+        element->ojciec = ojciec;
+        if (ojciec->lewo == NULL)
+        {
+            ojciec->lewo = element;
+        }
+        else
+        {
+            ojciec->prawo = element;
+        }
+        element2 = element;
+        while (element2->ojciec != NULL)
+        {
+            if (element2->ojciec->liczba < element2->liczba)
+            {
+                liczba = element2->ojciec->liczba;
+                element2->ojciec->liczba = element2->liczba;
+                element2->liczba = liczba;
+            }
+            else
+            {
+                break;
+            }
+            element2 = element2->ojciec;
+        }
+    }
+}
+
+//FUNKCJA NA USUNIECIE PIERWSZEGO(MAKSYMALNEGO WEZLA)
+struct Stog *UsunPierwszyWezel(struct Kopiec *korzen)
+{
+    struct Stog *element = NULL;
+    struct Stog *element2;
+    int liczba;
+    if (korzen->poziom >= 0)
+    {
+        element = korzen->wysokosc[korzen->poziom][korzen->IloscElementow -1];
+        korzen->wysokosc[korzen->poziom][korzen->IloscElementow -1] = NULL;
+        korzen->IloscElementow--;
+        if (element->ojciec)
+        {
+            if (element->ojciec->prawo != NULL)
+            {
+                element->ojciec->prawo = NULL;
+            }
+            else
+            {
+                element->ojciec->lewo = NULL;
+            }
+        }
+        
+        if (korzen->IloscElementow == 0)
+        {
+            Usungalaz(korzen, korzen->poziom);
+        }
+        
+        if (korzen->poziom >= 0)
+        {
+            liczba = korzen->wysokosc[0][0]->liczba;
+            korzen->wysokosc[0][0]->liczba = element->liczba;
+            element->liczba = liczba;
+
+            element2 = korzen->wysokosc[0][0];
+            while (element2)
+            {
+                if ((element2->prawo != NULL && element2->prawo->liczba > element2->liczba) || (element2->lewo != NULL && element2->lewo->liczba > element2->liczba))
+                {
+                    if (element2->prawo && element2->prawo->liczba > element2->lewo->liczba)
+                    {
+                        liczba = element2->liczba;
+                        element2->liczba = element2->prawo->liczba;
+                        element2->prawo->liczba = liczba;
+                        element2 = element2->prawo;
+                    }
+                    else
+                    {
+                        liczba = element2->liczba;
+                        element2->liczba = element2->lewo->liczba;
+                        element2->lewo->liczba = liczba;
+                        element2 = element2->lewo;
+                    }
+                }
+                else
+                {
+                    element2 = NULL;
+                }
+            }
+        }
+    }
+    return element;
+};
+
+//MAIN
+int main()
+{    
+    struct Kopiec *dana;
+    struct Stog *element;
+    
+    //DEKLARACJA KOPCA O WIELKOSCI 10 ELEMENTOW
+    dana = DrzewoKopiec(10);
+    
+    int opcja, wartosc;
+    int petla = 1;
+    
+    //MENU 
+    while (petla)
+	{
+        cout << "KOPIEC" << endl;
+		cout << "Wybierz operacje:" << endl;
+    	cout << "1. Dodaj wezel z klawiatury." << endl;
+    	cout << "2. Usun maksymalny wezel." << endl;
+    	cout << "3. Wyswietl kopiec." << endl;
+   		cout << "4. Wypisanie wezlow drzewa w kolejnosci VLR, LVR, LRV." << endl;
+   		cout << "5. Wyczysc konsole" << endl;
+   		cout << "0. Koniec dzialania." << endl;
+		
+		cin >> opcja;
+		
+        switch (opcja)
+        {
+            case 0: //KONIEC PROGRAMU
+            		cout << "Koniec pracy programu." << endl;
+            		exit(0);
+                	break;
+                	
+            case 1: //DODAWANIE DO WEZLA Z KLAWIATURY
+               	 	cout << "1. Dodawanie wezla\n\nPodaj liczbe:" << endl;
+                	scanf("%d", &wartosc);
+                	DodajDoKopca(dana, heap(wartosc));
+                	break;
+                	
+            case 2: //USUWANIE MAKSYMALNEGO WEZLA
+                	printf("Usun wezel max kopca: ");
+                	element = UsunPierwszyWezel(dana);
+                	if(element != NULL)
+                	{
+                    	printf("%d ", element->liczba);
+                	}
+                	break;
+            		
+            case 3: //WYSWIETLANIE KOPCA
+            		cout << "3. Wyswietl kopiec" << endl << endl;
+            		Wyswietl(dana);
+            		break;
+            		
+            case 4: //WYPISYWANIE WEZLOW KOPCA W KOLEJNOSCI VLR, LVR, LRV
+            		cout << "4. Wypisanie wezlow kopca w kolejnosci VLR, LVR, LRV." << endl << endl;
+                	cout << "VLR - pre-order, przejscie wzdluzne, prefiksowe" << endl;
+                	RodzajWyswietlania(dana,1);
+                	cout << endl;
+    				cout << endl << "LVR - in-order, przejscie poprzeczne, infiksowe" << endl;
+                	RodzajWyswietlania(dana,2);
+                	cout << endl;
+    				cout << endl << "LRV - post-oder, przejscie wsteczne, postfiksowe" << endl;
+                	RodzajWyswietlania(dana,3);
+                	cout << endl << endl;
+                	break;
+            case 5: //WYCZYSZCZENIE KONSOLI
+					system("cls");
+					break;
+            default:
+            		cout << "Wpisano bledny numer operacji. Sprobuj ponownie." << endl;
+                	break;
+        }
+    };
+    
+    return 0;
+}
